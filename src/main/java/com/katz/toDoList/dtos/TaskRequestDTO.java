534b804ec2dc1f41c2b6410697b792050bb88f72
Paulo Henrique <varelapaulo852@gmail.com>
2026-06-10 17:20:21 -0300

    Date update

--- src/main/java/com/katz/toDoList/dtos/TaskRequestDTO.java
+++ src/main/java/com/katz/toDoList/dtos/TaskRequestDTO.java
@@ -1,5 +1,6 @@
 package com.katz.toDoList.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.katz.toDoList.enums.TaskStatus;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,5 +11,5 @@ import java.time.LocalDateTime;
 public record TaskRequestDTO(
         @NotBlank(message = "Título é obrigatório") @Size(max = 100) String title,
         @Size(max = 500) String description,
-        @NotNull(message = "Horário é obrigatório") LocalDateTime taskDate,
+        @NotNull(message = "Horário é obrigatório") @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime taskDate,
         TaskStatus taskStatus) {}
--- src/main/java/com/katz/toDoList/dtos/TaskResponseDTO.java
+++ src/main/java/com/katz/toDoList/dtos/TaskResponseDTO.java
@@ -1,12 +1,13 @@
 package com.katz.toDoList.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.katz.toDoList.enums.TaskStatus;
 
 import java.time.LocalDateTime;
 
 public record TaskResponseDTO(String title,
                               String description,
-                              LocalDateTime taskDate,
+                              @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime taskDate,
                               String id,
                               LocalDateTime createdAt,
                               TaskStatus taskStatus) {}
