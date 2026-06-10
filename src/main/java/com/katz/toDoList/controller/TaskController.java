8e89a577fc3eaf3c90f2aa8a66297467a2d13e34
Paulo Henrique <varelapaulo852@gmail.com>
2026-06-10 16:55:29 -0300

    UpdateTask implementation

---
+++ src/main/java/com/katz/toDoList/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.katz.toDoList.controller;
+
+import com.katz.toDoList.dtos.TaskRequestDTO;
+import com.katz.toDoList.dtos.TaskResponseDTO;
+import com.katz.toDoList.services.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping("/createTask")
+    public ResponseEntity<TaskResponseDTO> createTask(@Valid @RequestBody TaskRequestDTO taskRequestDTO) {
+        TaskResponseDTO newTask = this.taskService.createTask(taskRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDTO>> getTask() {
+        return ResponseEntity.status(HttpStatus.OK).body(taskService.getTasks());
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable String id, @RequestBody TaskRequestDTO taskRequestDTO) {
+        TaskResponseDTO updatedTask = this.taskService.updateTask(id, taskRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedTask);
+    }
+
+}
--- src/main/java/com/katz/toDoList/services/TaskService.java
+++ src/main/java/com/katz/toDoList/services/TaskService.java
@@ -3,10 +3,13 @@ package com.katz.toDoList.services;
 import com.katz.toDoList.dtos.TaskRequestDTO;
 import com.katz.toDoList.dtos.TaskResponseDTO;
 import com.katz.toDoList.enums.TaskStatus;
+import com.katz.toDoList.exceptions.TaskNotFoundException;
 import com.katz.toDoList.models.Task;
 import com.katz.toDoList.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -20,6 +23,31 @@ public class TaskService {
         task.setDescription(taskRequestDTO.description());
         task.setTaskDate(taskRequestDTO.taskDate());
         task.setTaskStatus(TaskStatus.PENDENT);
+        Task savedTask = taskRepository.save(task);
+
+        return new TaskResponseDTO(
+                savedTask.getTitle(),
+                savedTask.getDescription(),
+                savedTask.getTaskDate(),
+                savedTask.getId(),
+                savedTask.getCreatedAt(),
+                savedTask.getTaskStatus());
+    }
+
+    public List<TaskResponseDTO> getTasks() {
+        List<Task> newTaskList = this.taskRepository.findAll();
+        return newTaskList.stream()
+                .map(task -> new TaskResponseDTO(task.getTitle(), task.getDescription(), task.getTaskDate(), task.getId(), task.getCreatedAt(), task.getTaskStatus()))
+                .toList();
+    }
+
+    public TaskResponseDTO updateTask(String id, TaskRequestDTO taskRequestDTO) {
+        Task task;
+        task = taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+        task.setTitle(taskRequestDTO.title());
+        task.setDescription(taskRequestDTO.description());
+        task.setTaskDate(taskRequestDTO.taskDate());
+        task.setTaskStatus(taskRequestDTO.taskStatus());
         Task savedTask = taskRepository.save(task);
 
         return new TaskResponseDTO(
